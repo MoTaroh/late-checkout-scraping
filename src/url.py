34c0ce8b61fe16d2690e08fb1811e6981ab4633a
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-05-12 15:10:54 +0000

    引数とベースURLを結合する関数

--- src/url.py
+++ src/url.py
@@ -1,3 +1,7 @@
 class Url:
     def __init__(self):
+        self.base_url = "https://www.jalan.net/"
         self.search_url = "https://hogehoge"
+
+    def generate_url(self, query):
+        self.search_url = self.base_url + query
--- test/test_url.py
+++ test/test_url.py
@@ -7,3 +7,20 @@ def test_url_is_str():
     url = Url()
     search_url = url.search_url
     assert type(search_url) is str
+
+
+def test_base_url():
+    url = Url()
+    base_url = url.base_url
+    assert base_url == "https://www.jalan.net/"
+
+
+def test_contact_url():
+    arg = "hello"
+    url = Url()
+    # 引数を入れたら結合してくれる
+    url.generate_url(arg)
+    search_url = url.search_url
+    # 期待値
+    expected_url = url.base_url + arg
+    assert search_url == expected_url

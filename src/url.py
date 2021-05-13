5329e12a171b962b5643a42d256e491ab0cbb4a2
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-05-13 15:19:06 +0000

    コンストラクタに移動

--- src/url.py
+++ src/url.py
@@ -1,7 +1,4 @@
 class Url:
-    def __init__(self):
+    def __init__(self, query):
         self.base_url = "https://www.jalan.net/"
-        self.search_url = "https://hogehoge"
-
-    def generate_url(self, query):
         self.search_url = self.base_url + query
--- test/test_url.py
+++ test/test_url.py
@@ -3,24 +3,25 @@ import pytest
 from src.url import Url
 
 
-def test_url_is_str():
-    url = Url()
+@pytest.fixture()
+def url():
+    query = "hello"
+    url = Url(query)
+    yield url
+
+
+def test_url_is_str(url):
     search_url = url.search_url
     assert type(search_url) is str
 
 
-def test_base_url():
-    url = Url()
+def test_base_url(url):
     base_url = url.base_url
     assert base_url == "https://www.jalan.net/"
 
 
-def test_contact_url():
+def test_constructer(url):
     arg = "hello"
-    url = Url()
-    # 引数を入れたら結合してくれる
-    url.generate_url(arg)
-    search_url = url.search_url
     # 期待値
     expected_url = url.base_url + arg
-    assert search_url == expected_url
+    assert url.search_url == expected_url

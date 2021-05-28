3c06eb7c4c349c1d3de2ceae796575de4d19a1e3
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-05-28 02:57:21 +0000

    日付のバリデーションを追加

--- src/stay_date.py
+++ src/stay_date.py
@@ -1,5 +1,14 @@
+import datetime
+
+
 class StayDate:
     def __init__(self, stayYear, stayMonth, stayDay):
+        # 引数チェック
+        staydate = datetime.date(stayYear, stayMonth, stayDay)
+        if staydate < datetime.date.today():
+            datetime.date.strftime
+            raise ValueError("Your staydate is outdated.")
+
         self.stayYear = str(stayYear)
         self.stayMonth = str(stayMonth)
         self.stayDay = str(stayDay)
--- test/test_staydate.py
+++ test/test_staydate.py
@@ -4,7 +4,12 @@ from src.stay_date import StayDate
 
 
 def test_staydate():
-    staydate = StayDate(stayYear=2021, stayMonth=5, stayDay=20)
-    assert staydate.stayYear == "2021"
+    staydate = StayDate(stayYear=2022, stayMonth=5, stayDay=20)
+    assert staydate.stayYear == "2022"
     assert staydate.stayMonth == "5"
     assert staydate.stayDay == "20"
+
+
+def test_staydate_is_outdated():
+    with pytest.raises(ValueError):
+        staydate = StayDate(stayYear=1990, stayMonth=5, stayDay=20)

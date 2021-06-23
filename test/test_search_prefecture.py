a1bdc1f895c31f6fc2b9aeb26201c42aee42ab17
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-06-23 10:35:12 +0000

    初期化処理を共通化

--- test/test_search_prefecture.py
+++ test/test_search_prefecture.py
@@ -3,22 +3,23 @@ import pytest
 from src.search_prefecture import SearchPrefecture
 
 
-def test_大阪の都道府県コード():
-    pref = SearchPrefecture("大阪")
-    assert pref.prefecture_code == "270000"
+class TestSearchPrefecture:
+    def setup_method(self, method):
+        self.pref = SearchPrefecture("大阪")
 
+    def test_大阪の都道府県コード(self):
+        assert self.pref.prefecture_code == "270000"
 
-def test_大阪の地域コード():
-    pref = SearchPrefecture("大阪")
-    osaka_region_code = [
-        "271400",
-        "271700",
-        "272000",
-        "272300",
-        "272600",
-        "272900",
-        "273200",
-    ]
+    def test_大阪の地域コード(self):
+        osaka_region_code = [
+            "271400",
+            "271700",
+            "272000",
+            "272300",
+            "272600",
+            "272900",
+            "273200",
+        ]
 
-    for index, region in enumerate(pref.region):
-        assert region["region_code"] == osaka_region_code[index]
+        for index, region in enumerate(self.pref.region):
+            assert region["region_code"] == osaka_region_code[index]

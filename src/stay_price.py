b28cb6dac6a32757ddf4147456b8db128ad4bb6d
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-01 14:28:51 +0000

    宿泊料金のバリデーションを追加

--- src/stay_price.py
+++ src/stay_price.py
@@ -1,7 +1,31 @@
 class Price:
-    def __init__(self, minPrice="", maxPrice=""):
+    def __init__(self, minPrice=0, maxPrice=""):
         self.minPrice = minPrice
         self.maxPrice = maxPrice
 
+        if self.maxPrice == "":
+            # 最小値の正の整数バリデーション
+            self.isPositive(self.minPrice)
+            self.isInteger(self.minPrice)
+        else:
+            # それぞれの正の整数バリデーション
+            self.isPositive(self.minPrice)
+            self.isPositive(self.maxPrice)
+            self.isInteger(self.minPrice)
+            self.isInteger(self.maxPrice)
+            # 最大料金 >= 最小料金のバリデーション
+            self.maxPrice_is_greater_than_minPrice()
+
+    def isPositive(self, price):
+        if price < 0:
+            raise ValueError("StayPrice must be positive value.")
+
+    def maxPrice_is_greater_than_minPrice(self):
         if self.minPrice > self.maxPrice:
             raise ValueError("minPrice is greater than maxPrice.")
+
+    def isInteger(self, price):
+        if isinstance(price, int):
+            pass
+        else:
+            raise ValueError("StayPrice must be integer.")
--- test/test_stay_price.py
+++ test/test_stay_price.py
@@ -6,3 +6,11 @@ class TestPrice:
     def test_最大料金が最小料金よりも小さければエラー(self):
         with pytest.raises(ValueError):
             self.price = Price(minPrice=10000, maxPrice=9000)
+
+    def test_宿泊料金が0円未満であればエラー(self):
+        with pytest.raises(ValueError):
+            self.price = Price(minPrice=-1)
+
+    def test_宿泊料金が小数であればエラー(self):
+        with pytest.raises(ValueError):
+            self.price = Price(minPrice=0.1)
